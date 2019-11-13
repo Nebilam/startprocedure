e716e87e30771ce9b29f1dcf16d6e7fa8990ac6d
Nebilam <49345234+Nebilam@users.noreply.github.com>
2019-11-13 19:44:59 +0100

    eerste commit

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    music.playTone(698, music.beat(BeatFraction.Double))
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})

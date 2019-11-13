7f8ba438a490184c5a50e5f96f71afa980c9d0f4
Nebilam <49345234+Nebilam@users.noreply.github.com>
2019-11-13 20:06:55 +0100

    foute wachttijd

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(500)
     pins.digitalWritePin(DigitalPin.P15, 1)
     music.playTone(349, music.beat(BeatFraction.Whole))
-    basic.pause(500)
+    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P16, 1)
     music.playTone(698, music.beat(BeatFraction.Double))
     basic.pause(2000)
